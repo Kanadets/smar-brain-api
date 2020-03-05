36d560e8b3396633ab8a9730d90ff6d7c2310092
Kanadets <fomindmitrii@icloud.com>
2020-03-04 22:29:42 -0800

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "74e3cc71c62341e2bcfbcd74c8ff4503"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => status(400).json("can't work with API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+};
